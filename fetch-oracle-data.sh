6262a18e93bdb99aceb6f2d3982663f07cd5e88d
Mateusz Czeladka <mateusz.szczap@gmail.com>
2020-12-28 00:39:18 +0100

    Update fetch-oracle-data.sh

--- fetch-oracle-data.sh
+++ fetch-oracle-data.sh
@@ -1,83 +1,97 @@
-#!/usr/bin/env bash                                                                                                                                                                                                                                                                                                    [7/845]
+#!/usr/bin/env bash
 set -e
-COINGECKOADAUSD=$(curl -s "https://api.coingecko.com/api/v3/coins/cardano?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.usd')
-ERGOPOOLADAUSD=$(curl -s "https://ada-usd-ergo-oracle.emurgo.io/frontendData" | sed -e 's/^.//' -e 's/.$//' -e 's/\\//g' | jq '.latest_price')
-ERGOPOOLERGUSD=$(curl -s "https://erg-usd-ergo-oracle.emurgo.io/frontendData" | sed -e 's/^.//' -e 's/.$//' -e 's/\\//g' | jq '.latest_price')
 
-COINGECKOADAEUR=$(curl -s "https://api.coingecko.com/api/v3/coins/cardano?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.eur')
+COIN_GECKO_CARDANO_JSON=$(curl -s 'https://api.coingecko.com/api/v3/coins/cardano?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+COIN_GECKO_BTC_JSON=$(curl -s 'https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+COIN_GECKO_AGI_JSON=$(curl -s 'https://api.coingecko.com/api/v3/coins/singularitynet?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+COIN_GECKO_ERGO_JSON=$(curl -s 'https://api.coingecko.com/api/v3/coins/ergo?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
 
-COINGECKOERGUSD=$(curl -s "https://api.coingecko.com/api/v3/coins/ergo?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.usd')
+CRYPTO_COMPARE_JSON=$(curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ADA,BTC,AGI&tsyms=BTC,USD,EUR&api_key=3ac2bcc4d5ca254f38c40d6da30feeccf5788d292a25f3f06b6242a4300bd352')
+ERGO_POOL_ADA_USD_JSON=$(curl -s "https://ada-usd-ergo-oracle.emurgo.io/frontendData" | sed -e 's/^.//' -e 's/.$//' -e 's/\\//g')
+ERGO_POOL_ERG_USD_JSON=$(curl -s "https://erg-usd-ergo-oracle.emurgo.io/frontendData" | sed -e 's/^.//' -e 's/.$//' -e 's/\\//g')
+MARKET_STACK_JSON=$(curl -s "http://api.marketstack.com/v1/intraday/latest?access_key=d7b2c35197724da1541bd0c484c6c1fa&symbols=TSLA");
+BTC_TREZOR_JSON=$(curl -s "https://btc1.trezor.io/api/v2")
 
-COINGECKOADABTC=$(curl -s "https://api.coingecko.com/api/v3/coins/cardano?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.btc')
-COINGECKOBTCUSD=$(curl -s "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.usd')
-
-TESLASTOCKPRICE=$(curl -s "http://api.marketstack.com/v1/intraday/latest?access_key=??????&symbols=TSLA" | jq '.data[].last');
-
-#BTCDIFF=$(curl -s "https://chain.api.btc.com/v3/block/latest" | jq '.data.difficulty')
-BTCDIFF=$(curl -s "https://btc1.trezor.io/api/v2" | jq -r '.backend.difficulty')
-
-COINGECKOAGIBTC=$(curl -s "https://api.coingecko.com/api/v3/coins/singularitynet?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false" | jq '.market_data.current_price.usd')
-DRAND=$( curl -s "https://api.drand.sh/public/latest" | jq 'del(.signature)' | jq 'del(.previous_signature)')
-
+DRAND=$(curl -s "https://api.drand.sh/public/latest" | jq 'del(.signature)' | jq 'del(.previous_signature)')
 
 echo "{"
 echo "  \"1968\": {"
 echo "    \"ADAUSD\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOADAUSD\","
+echo "         \"value\": \"$(jq '.market_data.current_price.usd' <<< "$COIN_GECKO_CARDANO_JSON")\","
 echo "         \"source\": \"coinGecko\""
 echo "       },"
 echo "       {"
-echo "         \"value\": \"$ERGOPOOLADAUSD\","
+echo "         \"value\": \"$(jq '.latest_price' <<< "$ERGO_POOL_ADA_USD_JSON")\","
 echo "         \"source\": \"ergoOracles\""
+echo "       }",
+echo "       {"
+echo "         \"value\": \"$(jq '.ADA.USD' <<< "$CRYPTO_COMPARE_JSON")\","
+echo "         \"source\": \"cryptoCompare\""
 echo "       }"
 echo "    ],"
 echo "    \"ADAEUR\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOADAEUR\","
+echo "         \"value\": \"$(jq '.market_data.current_price.eur' <<< "$COIN_GECKO_CARDANO_JSON")\","
 echo "         \"source\": \"coinGecko\""
+echo "       }",
+echo "       {"
+echo "         \"value\": \"$(jq '.ADA.EUR' <<< "$CRYPTO_COMPARE_JSON")\","
+echo "         \"source\": \"cryptoCompare\""
 echo "       }"
 echo "    ],"
 echo "    \"ADABTC\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOADABTC\","
+echo "         \"value\": \"$(jq '.market_data.current_price.btc' <<< "$COIN_GECKO_CARDANO_JSON")\","
 echo "         \"source\": \"coinGecko\""
+echo "       },"
+echo "       {"
+echo "         \"value\": \"$(jq '.ADA.BTC' <<< "$CRYPTO_COMPARE_JSON")\","
+echo "         \"source\": \"cryptoCompare\""
 echo "       }"
 echo "    ],"
 echo "    \"BTCUSD\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOBTCUSD\","
+echo "         \"value\": \"$(jq '.market_data.current_price.usd' <<< "$COIN_GECKO_BTC_JSON")\","
 echo "         \"source\": \"coinGecko\""
+echo "       }",
+echo "       {"
+echo "         \"value\": \"$(jq '.BTC.USD' <<< "$CRYPTO_COMPARE_JSON")\","
+echo "         \"source\": \"cryptoCompare\""
 echo "       }"
 echo "    ],"
-echo "    \"AGIBTC\": ["
+echo "    \"AGIUSD\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOAGIBTC\","
+echo "         \"value\": \"$(jq '.market_data.current_price.usd' <<< "$COIN_GECKO_AGI_JSON")\","
 echo "         \"source\": \"coinGecko\""
+echo "       }",
+echo "       {"
+echo "         \"value\": \"$(jq '.AGI.USD' <<< "$CRYPTO_COMPARE_JSON")\","
+echo "         \"source\": \"cryptoCompare\""
 echo "       }"
 echo "    ],"
 echo "    \"BTCDIFF\": ["
 echo "       {"
-echo "         \"value\": \"$BTCDIFF\","
+echo "         \"value\": \"$(jq -r '.backend.difficulty' <<< "$BTC_TREZOR_JSON")\","
 echo "         \"source\": \"blockBook\""
 echo "       }"
 echo "    ],"
 echo "    \"ERGUSD\": ["
 echo "       {"
-echo "         \"value\": \"$COINGECKOERGUSD\","
+echo "         \"value\": \"$(jq '.market_data.current_price.usd' <<< "$COIN_GECKO_ERGO_JSON")\","
 echo "         \"source\": \"coinGecko\""
 echo "       },"
 echo "       {"
-echo "         \"value\": \"$ERGOPOOLERGUSD\","
+echo "         \"value\": \"$(jq '.latest_price' <<< "$ERGO_POOL_ERG_USD_JSON")\","
 echo "         \"source\": \"ergoOracles\""
 echo "       }"
 echo "    ],"
 echo "    \"TSLA\": ["
 echo "       {"
-echo "         \"value\": \"$TESLASTOCKPRICE\","
+echo "         \"value\": \"$(jq '.data[].last' <<< "$MARKET_STACK_JSON")\","
 echo "         \"source\": \"investorsExchange\""
 echo "       }"
 echo "    ],"
 echo "    \"DRAND\": $DRAND"
 echo "  }"
-echo "}"
+echo "}"⏎
